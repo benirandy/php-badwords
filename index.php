9a8cafbf4ae9e36bae773a07986be58a849dae34
Beni <haroldbeni111997@gmail.com>
2023-04-18 16:08:36 +0200

    proseguimento

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Badwords</title>
 </head>
 <body>
-    CIAO
+    
+<form action="censura.php" method="GET">
+<label for="paragrafo">Paragrafo da censurare:</label>
+<input type="text" id="paragrafo"name="paragrafo"/>
+
+<label for="censura">Paragrafo da censurare:</label>
+<input type="text" id="censura"name="censura"/>
+
+<button type="type">Enter</button>
+</form>
+
 </body>
 </html>
